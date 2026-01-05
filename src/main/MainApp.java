d627b64b75320258a63edf5edd274628c6dbcb73
Your Name <your_email@example.com>
2026-01-05 21:09:42 +0700

    update

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -2,6 +2,8 @@ package main;
 
 import controller.HomeController;
 import view.HomeView;
+import view.LoginView;
+
 import javax.swing.*;
 
 public class MainApp {
@@ -10,6 +12,7 @@ public class MainApp {
         // 2. Khởi chạy ứng dụng trên luồng sự kiện (Chuẩn Java Swing)
         SwingUtilities.invokeLater(() -> {
             // Tạo giao diện trang chủ (nhưng chưa hiện)
+            LoginView login = new LoginView();
             HomeView view = new HomeView();
 
             // Kích hoạt Controller để xử lý logic và load dữ liệu
